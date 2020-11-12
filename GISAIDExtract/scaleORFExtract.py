4a2c0f79a12b9cfb44636484bf768c7b605a9218
Lucy Paddock <lucinda.paddock@gmail.com>
2020-11-11 19:54:12 -0500

    function to find Z scores for a given scoreDict

--- GISAIDExtract/scaleORFExtract.py
+++ GISAIDExtract/scaleORFExtract.py
@@ -5,6 +5,7 @@ Description : scaleORFExtract.py for covid biomakerspace project
 """
 
 import pandas as pd
+import numpy as np
 from ORFExtracter import *
 from Bio.Seq import Seq
 from Bio.SeqRecord import SeqRecord
@@ -51,7 +52,59 @@ def scaleOrfAlignScore(BigFastaAddress, scoreDict={}):
             orfInfo, columns=['AscensionNum', 'AlignmentScore', 'Location'])
         currentCSVName = orf + 'AlignmentScore.csv'
         currentDF.to_csv(currentCSVName)
-    return
+    return scoreDict
+
+def ZScore(scoreDict):
+    '''idea: make scoreDict a dictionary of dictionaries. But besides that, this
+    function takes in the scoreDict produced from our GISAID file and adds
+    an entry to each sequence info tuple of that sequence's Z score relative
+    to the rest of its ORF'''
+    #loop through orfs
+    for orf in scoreDict:
+
+        orfL = scoreDict[orf]
+        scoreL = []
+
+        #loop through each sequence sample of one orf
+        for seqT in orfL:
+            #extract the alignment score from dict
+            scoreL.append(seqT[1]) 
+        mean = np.mean(scoreL)
+        std = np.std(scoreL)
+        print(mean)
+        print(std)
+
+        #update sequnce tuples with Z score
+        newOrfL = []
+        for seqT in orfL:
+            ZScore = (seqT[1]-mean)/std
+            newOrfL.append((seqT[0],seqT[1],ZScore,seqT[2]))
+        scoreDict[orf] = newOrfL
+
+    return scoreDict
+
+'''
+Tests
+scoreDict = {'ORF1a': [('IVDC-HB-01', 13230.0, 'Wuhan'), ('IVDC-HB-04', 13230.0, 'Wuhan'), ('IVDC-HB-05', 13230.0, 'Wuhan'), ('IPBCAMS-WH-01', 13227.0, 'Wuhan'), ('WIV04', 13230.0, 'Wuhan')], 
+'ORF1b': [('IVDC-HB-01', 8106.0, 'Wuhan'), ('IVDC-HB-04', 8106.0, 'Wuhan'), ('IVDC-HB-05', 8104.0, 'Wuhan'), ('IPBCAMS-WH-01', 8106.0, 'Wuhan'), ('WIV04', 8106.0, 'Wuhan')],
+'ORFS': [('IVDC-HB-01', 3882.0, 'Wuhan'), ('IVDC-HB-04', 3882.0, 'Wuhan'), ('IVDC-HB-05', 3882.0, 'Wuhan'), ('IPBCAMS-WH-01', 3885.0, 'Wuhan'), ('WIV04', 3882.0, 'Wuhan')],
+'ORF3a': [('IVDC-HB-01', 873.0, 'Wuhan'), ('IVDC-HB-04', 873.0, 'Wuhan'), ('IVDC-HB-05', 873.0, 'Wuhan'), ('IPBCAMS-WH-01', 873.0, 'Wuhan'), ('WIV04', 874.0, 'Wuhan')], 
+'ORFN': [('IVDC-HB-01', 1302.0, 'Wuhan'), ('IVDC-HB-04', 1301.0, 'Wuhan'), ('IVDC-HB-05', 1302.0, 'Wuhan'), ('IPBCAMS-WH-01', 1302.0, 'Wuhan'), ('WIV04', 1302.0, 'Wuhan')]}
+
+ZScore(scoreDict) returns
+{'ORFS': [('IVDC-HB-01', 3882.0, -0.4999999999999242, 'Wuhan'), ('IVDC-HB-04', 3882.0, -0.4999999999999242, 'Wuhan'), ('IVDC-HB-05', 3882.0, -0.4999999999999242, 'Wuhan'), ('IPBCAMS-WH-01', 3885.0, 2.000000000000076, 'Wuhan'), ('WIV04', 3882.0, -0.4999999999999242, 'Wuhan')],
+'ORF1b': [('IVDC-HB-01', 8106.0, 0.4999999999995453, 'Wuhan'), ('IVDC-HB-04', 8106.0, 0.4999999999995453, 'Wuhan'), ('IVDC-HB-05', 8104.0, -2.0000000000004547, 'Wuhan'), ('IPBCAMS-WH-01', 8106.0, 0.4999999999995453, 'Wuhan'), ('WIV04', 8106.0, 0.4999999999995453, 'Wuhan')], 
+'ORF3a': [('IVDC-HB-01', 873.0, -0.5000000000001137, 'Wuhan'), ('IVDC-HB-04', 873.0, -0.5000000000001137, 'Wuhan'), ('IVDC-HB-05', 873.0, -0.5000000000001137, 'Wuhan'), ('IPBCAMS-WH-01', 873.0, -0.5000000000001137, 'Wuhan'), ('WIV04', 874.0, 1.9999999999998863, 'Wuhan')], 
+'ORF1a': [('IVDC-HB-01', 13230.0, 0.5000000000003032, 'Wuhan'), ('IVDC-HB-04', 13230.0, 0.5000000000003032, 'Wuhan'), ('IVDC-HB-05', 13230.0, 0.5000000000003032, 'Wuhan'), ('IPBCAMS-WH-01', 13227.0, -1.999999999999697, 'Wuhan'), ('WIV04', 13230.0, 0.5000000000003032, 'Wuhan')], 
+'ORFN': [('IVDC-HB-01', 1302.0, 0.5000000000001136, 'Wuhan'), ('IVDC-HB-04', 1301.0, -1.9999999999998859, 'Wuhan'), ('IVDC-HB-05', 1302.0, 0.5000000000001136, 'Wuhan'), ('IPBCAMS-WH-01', 1302.0, 0.5000000000001136, 'Wuhan'), ('WIV04', 1302.0, 0.5000000000001136, 'Wuhan')]}
+'''
+
+
+
+
+
+
+
 
 # def scaleExtract(fastaAddress):
 #     """extract location, ascensionNum, date, sequence info from gisaid fasta file"""
